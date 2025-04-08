8d02837a5fdf3dd0112f9445443916647dfbae48
tanya8800 <tanyamariedelacruz08@gmail.com>
2025-04-08 12:33:14 +0800

    initial commit

--- server/routes.js
+++ server/routes.js
@@ -11,20 +11,23 @@ router.get("/todos", async (req, res) => {
     res.status(200).json(todos);
 });
 
-// POST /todos
-router.post("/todos", async (req, res) => {
-    const collection = getCollection();
-    let { todo } = req.body;
-
-  
-    const newTodo = await collection.insertOne({ todo, status: false });
-  
-    res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
-  })
-
 // DELETE /todos/:id
-router.delete("/todos/:id", (req, res) => {
-    res.status(200).json({ mssg: "DELETE REQUEST TO /api/todos" });
+router.delete("/todos/:id", async (req, res) => {
+  const collection = getCollection();
+  const _id = new ObjectId(req.params.id);
+
+  const deletedTodo = await collection.deleteOne({ _id });
+  res.status(200).json(deletedTodo);
+})
+
+// PUT /todos/:id
+router.put("/todos/:id", async (req, res) => {
+  const collection = getCollection();
+  const _id = new ObjectId(req.params.id);
+  const { status } = req.body;
+
+  const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
+  res.status(200).json(updatedTodo);
 });
 
 // PUT /todos/:id
