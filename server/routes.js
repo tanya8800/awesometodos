6f5c7677b6898a707dd483533e6f5ddb63e694dc
tanya8800 <tanyamariedelacruz08@gmail.com>
2025-04-08 12:47:28 +0800

    initial commit

--- server/routes.js
+++ server/routes.js
@@ -5,10 +5,10 @@ const { ObjectId } = require("mongodb");
 
 // GET /todos
 router.get("/todos", async (req, res) => {
-    const collection = getCollection();
-    const todos = await collection.find({}).toArray();
+  const collection = getCollection();
+  const todos = await collection.find({}).toArray();
 
-    res.status(200).json(todos);
+  res.status(200).json(todos);
 });
 
 // DELETE /todos/:id
@@ -26,22 +26,23 @@ router.put("/todos/:id", async (req, res) => {
   const _id = new ObjectId(req.params.id);
   const { status } = req.body;
 
+  if (typeof status !== "boolean") {
+      return res.status(400).json({ mssg: "invalid status" });
+  }
+
   const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
   res.status(200).json(updatedTodo);
 });
 
-// PUT /todos/:id
-router.put("/todos/:id", async (req, res) => {
-    const collection = getCollection();
-    const _id = new ObjectId(req.params.id);
-    const { status } = req.body;
+// POST /todos
+router.post("/todos", async (req, res) => {
+  const collection = getCollection();
+  let { todo } = req.body;
 
-    if (typeof status !== "boolean") {
-        return res.status(400).json({ mssg: "invalid status" });
-    }
-  
-    const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
-    res.status(200).json(updatedTodo);
-  });
 
+
+  const newTodo = await collection.insertOne({ todo, status: false });
+
+  res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
+})
 module.exports = router;
